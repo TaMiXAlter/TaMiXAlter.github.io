415f1eb8484bbbce2c5d37fecd219cc1cd7c64d4
TaMiXAlter <92863940+TaMiXAlter@users.noreply.github.com>
2023-01-11 12:14:22 +0800

    Add files via upload

---
+++ final exam/js/test.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(".n1").click(function(){
+       $(".one img").slideToggle()
+    })
+
+    $(".n2").click(function(){
+        $(".two img").slideToggle()
+     })
+
+     $(".n3").click(function(){
+        $(".three img").slideToggle()
+     })
+
+     $(".title1").click(function(){
+        $(".text1").slideToggle()
+     })
+
+     $(".title2").click(function(){
+        $(".text2").slideToggle()
+     })
+
+     $(".title3").click(function(){
+        $(".text3").slideToggle()
+     })
+
+
+})
